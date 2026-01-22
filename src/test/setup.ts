658dceee10166b2fc51c653c19addbf344649cb4
Hiago Bahú <hiagobahu1@gmail.com>
2026-01-22 18:02:54 -0300

    chore: added matchMedia mock to be able to test themes

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,18 @@
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+if (globalThis.window !== undefined) {
+  Object.defineProperty(globalThis.window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  })
+}
